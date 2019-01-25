4bbbe0d40239815e7cc4ee16a720e7d5dc67036f
carly <carly@noukod.com>
2019-01-25 17:40:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190125174021) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "published_status"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "published_status"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "published_status"
+  end
 
 end

629e8b8fc45a5a3c88be00957d2a92c55a7f67c7
carly <carly@noukod.com>
2019-01-25 17:36:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :genres
+  resources :artists
+  resources :songs
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
